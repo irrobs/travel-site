73ed6f32f3c4a7341509ab91ea62bc2425c2d66b
Matheus Borri <matheus.escobar90@gmail.com>
2023-11-16 22:59:16 -0300

    completed reveal on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,11 @@
 import "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
+import Reveal from "./modules/Reveal";
+
+new Reveal(document.querySelectorAll(".feature"), 75);
+new Reveal(document.querySelectorAll(".testimonial"), 60);
+let mobileMenu = new MobileMenu();
 
 if (module.hot) {
   module.hot.accept();
 }
-
-let mobileMenu = new MobileMenu();
