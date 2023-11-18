4df0d2aceb0ea16ca3a079d23ba292c4e302f314
Matheus Borri <matheus.escobar90@gmail.com>
2023-11-18 01:58:43 -0300

    finish modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -6,7 +6,24 @@ import StickyHeader from "./modules/StickyHeader";
 new Reveal(document.querySelectorAll(".feature"), 75);
 new Reveal(document.querySelectorAll(".testimonial"), 60);
 const mobileMenu = new MobileMenu();
-const stickyHeader = new StickyHeader();
+new StickyHeader();
+let modal;
+
+document.querySelectorAll(".open-modal").forEach((el) =>
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (typeof modal === "undefined") {
+      import(/* webpackChunkName: "modal" */ "./modules/Modal")
+        .then((x) => {
+          modal = new x.default();
+          setTimeout(() => modal.openTheModal(), 20);
+        })
+        .catch(() => console.log("problem"));
+    } else {
+      modal.openTheModal();
+    }
+  })
+);
 
 if (module.hot) {
   module.hot.accept();
