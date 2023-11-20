37266140fe700d9bf4954c006a9da0b229711624
Matheus Borri <matheus.escobar90@gmail.com>
2023-11-19 23:54:28 -0300

    added client are

--- app/assets/scripts/modules/ClientArea.js
+++ app/assets/scripts/modules/ClientArea.js
@@ -1,6 +1,36 @@
+import Axios from "axios";
+
 class ClientArea {
   constructor() {
     this.injectHTML();
+    this.form = document.querySelector(".client-area__form");
+    this.field = document.querySelector(".client-area__input");
+    this.contentArea = document.querySelector(".client-are__content-area");
+
+    this.events();
+  }
+
+  events() {
+    this.form.addEventListener("submite", (e) => {
+      e.preventDefault();
+      this.sendRequest();
+    });
+  }
+
+  sendRequest() {
+    Axios.post(
+      "https://serene-sopapillas-e14518.netlify.app/.netlify/functions/secret-area",
+      { password: this.field.value }
+    )
+      .then((response) => {
+        this.form.remove();
+        this.contentArea.innerHTML = response.data;
+      })
+      .catch(() => {
+        this.contentArea.innerHTML = `<p class="client-area__error > Wrong password</p>`;
+      });
+    this.field.value = "";
+    this.field.focus();
   }
 
   injectHTML() {
